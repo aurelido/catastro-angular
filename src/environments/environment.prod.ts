d8b657b01837f813cec42252f494fcb57b71b05f
Aurelio Aragones Banegas <aurelio.aragones@gmail.com>
2018-02-18 02:35:58 +0000

    Removing Google API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://agile-reaches-32584.herokuapp.com/api',
-  googleMapsKey: process.env.GOOGLE_MAP_KEY
+  api_url: 'https://agile-reaches-32584.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,6 @@
 
 export const environment = {
   production: false,
-  api_url: 'http://localhost:3000/api',
-  googleMapsKey: process.env.GOOGLE_MAP_KEY
+  api_url: 'http://localhost:3000/api'
 };
 
